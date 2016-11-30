5fca6ee02f9aa4befccefcac3a03c4a31cd6d1ad
Wenzhao Zhang <wzhang87@hawk.iit.edu>
2016-11-30 15:09:30 -0800

    api

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # project3
 
-Jingwen Yu:
 Primary strength
 
+Jingwen Yu:
 My strength is to finish things on time.
 
 Wenzhao Zhang:
-
-Primary strength
-
-My strength is to My strength is to utilize what I've learned to build things on it.
+My strength is to utilize what I've learned to build things on it.
--- site.js
+++ site.js
@@ -1,36 +1,39 @@
-$(document).ready(function(){
-    $("#flip").click(function(){
-        $(".slideshowbody").slideToggle("slow");
-    });
+$(document).ready(function() {
+  $('#zipform').on('submit', function(event) {
+    var loc = $('#zipcode').val();
+    var lat;
+    var lng;
+    $.get(
+      'https://www.zipcodeapi.com/rest/js-rTyMicsfWrUvifDkyeNHgt1EysudYy0pszcKLFZH6Udp9QJI5wqlZs0yUuMS3Niw/info.json/'+loc+'/degrees',
+      function (data) {
+        lat = Math.round(data.lat);
+        lng = Math.round(data.lng);
+        $('#print').append(
+        document.createTextNode(
+"https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=find+auto+shop+around+you&rflfq=1&rlha=0&rllag=+" + lat + "," +lng +"&tbm=lcl&tbs=lf:1,lf_ui:2,lf_pqs:EAE")
+    );
+        document.write("<a href=\"https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=find+auto+shop+around+you&rflfq=1&rlha=0&rllag=+" + lat + "," +lng +"&tbm=lcl&tbs=lf:1,lf_ui:2,lf_pqs:EAE\">");
+        document.write("Your Title");
+        document.write("</a>");
+      });
+      event.preventDefault();
+  });
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function(){
+  $("#brand").mouseenter(function(){
+    $("#dropdown").slideDown();
+  });
+  $("#dropdown").mouseleave(function(){
+    $("#dropdown").slideUp();
+  });
+});
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("currentSlide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length){
-    slideIndex = 1
-  } 
-  if (n < 1){
-    slideIndex = slides.length
+function formFunction() {
+    if (confirm("Press Yes if you want to send this to us") == true) {
+        window.confirm("You submission has been sent successfully!");
+    } else {
+        window.alert("You pressed Cancel :C");
     }
-  for (a = 0; a < slides.length; a++){
-    slides[a].style.display = "none"; 
-  }
-  for (b = 0; b < dots.length; b++) {
-      dots[b].className = dots[b].className.replace(" activation", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " activation";
+    formFunction.preventDefault();
 }
